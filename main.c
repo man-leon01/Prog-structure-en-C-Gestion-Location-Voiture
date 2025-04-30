b5229d9ae6ddb11f6b3552def65b722d9b6c3a3e
Man-Leon <manleon115@gmail.com>
2025-04-30 22:33:14 +0000

    Start Project

---
+++ main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Variables Globales
+int choix;
+// Les constantes : char max,
+#define CMAX 20
+
+// Les Structures
+typedef struct {
+    char Marque[CMAX];
+    int Annee;
+    char Matricule[CMAX];
+    int Kilometrage;
+    char Etat[CMAX];
+} Voiture;
+
+typedef struct {
+    char Nom[CMAX];
+    char Adresse[CMAX];
+    char Identite[CMAX];
+    int Telephone;
+} Client;
+
+typedef struct {
+    char Identite_Client[CMAX];
+    char Matricule_Voiture[CMAX];
+    int Date_Location;
+    int Date_Retour;
+    float Montant;
+    float Avance;
+    float Reste;
+} Location;
+
+typedef struct {
+    char Login_Admin[CMAX];
+    int Mot_de_passe;
+} Securite;
+
+void Menu(){
+    do {
+        printf("\n     === MENU ===\n\n");
+        printf("   1. Gestion des Voitures\n");
+        printf("   2. Gestion des Clients\n");
+        printf("   3. Gestion des Locations\n");
+        printf("   4. QUITTER\n");
+        printf("                               choix : ");
+        scanf("%d", &choix);
+
+        switch (choix) {
+            case 1: GestionVoitures(); break;
+            case 2: GestionClients(); break;
+            case 3: GestionLocations(); break;
+            case 4: Quitter(); break;
+            case 0: printf("Choix Invalide!!!"); Menu(); break;
+            default: printf("Choix Invalide!!!\n");
+        }
+
+    } while (choix != 0);
+}
+void GestionVoitures(){
+    printf("\nFonction de Gestion des Voitures\n");
+}
+void GestionClients(){
+    printf("\nFonction de Gestion des Clients\n");
+}
+void GestionLocations(){
+    printf("\nFonction de Gestion des Locations\n");
+}
+void Quitter(){
+    printf("\nQuitter le Programme\n");
+}
+
+
+
+int main()
+{
+    printf("\n  ===EPL-LOCATIONS-CAR===\n");
+    Menu();
+    return 0;
+}
