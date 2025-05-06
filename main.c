830299d14ce5411ec6481fcfa1acda68dc8a77db
man-leon01 <manleon115@gmail.com>
2025-05-06 23:34:57 +0000

    Update main.c

--- main.c
+++ main.c
@@ -49,50 +49,57 @@ void Menu(){
         printf("   4. QUITTER\n");
         printf("                               choix : ");
         scanf("%d", &choix);
-        while ((choix = getchar()) != '\n' && choix != EOF);
+
+        // Consommer le caractère de nouvelle ligne restant dans le buffer
+        while (getchar() != '\n' && choix != EOF);
 
         switch (choix) {
             case 1: GestionVoitures(); break;
             case 2: GestionClients(); break;
             case 3: GestionLocations(); break;
             case 4: Quitter(); break;
-            case 0: printf("Choix Invalide!!!"); Menu(); break;
-            default: printf("Choix Invalide!!!\n");
+            default: printf("Choix Invalide!!!\n"); break;
         }
 
-    } while (choix != 0);
+    } while (choix != 4); // Sort de la boucle lorsque l'on choisit de quitter
 }
+
 void GestionVoitures(){
     printf("\nFonction de Gestion des Voitures\n");
+    printf("Retour au Menu Principal\n");
 }
+
 void GestionClients(){
     printf("\nFonction de Gestion des Clients\n");
+    printf("Retour au Menu Principal\n");
 }
+
 void GestionLocations(){
     printf("\nFonction de Gestion des Locations\n");
+    printf("Retour au Menu Principal\n");
 }
+
 void Quitter(){
     printf("\n     ===Quitter le Programme===\n");
-    printf("Voulez-vous vraiment quitter le programme ?\nSi oui appuyer sur 1 ou 'y'\nSi non appuyer sur autre caractère\n");
+    printf("\nVoulez-vous vraiment quitter le programme ?\nSi oui appuyer sur 1 ou 'y'\nSi non appuyer sur autre caractère\n");
     printf("\nEntrez votre reponse : ");
     char ch;
-    ch = getchar();
-    if (ch == '1' || ch == 'y'){
-        return 0;
-    }else{
+    ch = getchar();  // Lit la réponse de l'utilisateur
+    if (ch == '1' || ch == 'y' || ch == 'Y'){
+        printf("\nMerci d'avoir utilisé le programme. À bientôt !\n");
+        exit(0); // Quitte le programme
+    } else {
         printf("\nRetour au Menu Principal\n");
-        Menu();
-        
+        Menu();  // Retour au menu
     }
-    
 }
-
-
 
 int main()
 {
-    setlocale(LC_ALL,"");
+    //setlocale(LC_ALL,"");
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
     printf("\n  ===EPL---LOCATIONS--CAR===\n");
-    Menu();
+    Menu(); // Appel de la fonction Menu pour commencer le programme
     return 0;
 }
